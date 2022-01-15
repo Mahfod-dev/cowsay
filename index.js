e823e6eea7cb5d0a9d18006b2373f26d03844024
Mahfod <mahfod.dev@gmail.com>
2022-01-15 20:32:19 +0100

    create file information and index add cowsay

---
+++ index.js
@@ -0,0 +1,5 @@
+const infoWild = require('./information.js')
+const cowsay = require('cowsay')
+
+//node index.js terminal
+console.log(cowsay.say(infoWild))
---
+++ information.js
@@ -0,0 +1,5 @@
+infoWild = {
+	text: "Hello I'm Mahfod on remote Wild campus",
+}
+
+module.exports = infoWild
